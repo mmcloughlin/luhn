b611e357da3a5e17abf9b8dfe40b111eb1cdc50d
Michael McLoughlin <mmcloughlin@gmail.com>
2015-06-22 23:16:34 -0700

    Using list() for python3

--- luhn.py
+++ luhn.py
@@ -5,7 +5,7 @@ def checksum(string):
     Compute the Luhn checksum for the provided string of digits. Note this
     assumes the check digit is in place.
     """
-    digits = map(int, string)
+    digits = list(map(int, string))
     odd_sum = sum(digits[-1::-2])
     even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
     return (odd_sum + even_sum) % 10
