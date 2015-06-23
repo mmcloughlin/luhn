3b253ff455268ac6a42cf124506ecadf41d39d5c
Michael McLoughlin <mmcloughlin@gmail.com>
2015-06-22 22:15:53 -0700

    Import README to long_description

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ import re
 module_file = open('luhn.py').read()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
 
+def readme():
+    return open('./README.rst').read()
+
 setup(name='luhn',
       version=metadata['version'],
       description='Generate and verify Luhn check digits',
+      long_description=readme(),
       url='https://github.com/mmcloughlin/luhn',
       author='Michael McLoughlin',
       author_email='mmcloughlin@gmail.com',
