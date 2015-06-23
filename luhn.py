1935930f202f8cb9d10638ca5edbc3ab1bca0b4f
Michael McLoughlin <mmcloughlin@gmail.com>
2015-06-22 22:49:53 -0700

    Docstrings for functions

--- luhn.py
+++ luhn.py
@@ -1,14 +1,34 @@
 __version__ = '0.1.0'
 
 def checksum(string):
+    """
+    Compute the Luhn checksum for the provided string of digits. Note this
+    assumes the check digit is in place.
+    """
     digits = map(int, string)
     odd_sum = sum(digits[-1::-2])
     even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
     return (odd_sum + even_sum) % 10
 
 def verify(string):
+    """
+    Check if the provided string of digits satisfies the Luhn checksum.
+
+    >>> verify('356938035643809')
+    True
+    >>> verify('534618613411236')
+    False
+    """
     return (checksum(string) == 0)
 
 def generate(string):
+    """
+    Generate the Luhn check digit to append to the provided string.
+
+    >>> generate('35693803564380')
+    9
+    >>> generate('53461861341123')
+    4
+    """
     cksum = checksum(string + '0')
     return (10 - cksum) % 10
